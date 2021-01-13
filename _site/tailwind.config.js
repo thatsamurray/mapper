3236f3cb105b147315d4ca81ebf7ab6d5c6deece
Adam Murray <admmrry@gmail.com>
2021-01-13 14:47:54 +0000

    Simplying code - styling in css

--- _site/tailwind.config.js
+++ _site/tailwind.config.js
@@ -2,7 +2,12 @@ module.exports = {
 	purge: ['./_site/**/*.html'],
   darkMode: false, // or 'media' or 'class'
 	theme: {
-		extend: {},
+		colors: {
+			cl__first: 'var(--cl-first)',
+			cl__second: 'var(--cl-second)'
+			cl__third: 'var(--cl-third)',
+			cl__fourth: 'var(--cl-fourth)'
+		}
 	},
 	variants: {},
 	plugins: [],
